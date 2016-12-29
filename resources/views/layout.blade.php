15edae1c8f36dd47abfa30b324c02ac5d8eaaf18
Iszlai Zoltan <ziszlai@gmail.com>
2016-12-29 14:38:01 +0200

    Layout organization

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    @yield('header')
+</head>
+<body>
+
+    @yield('content')
+
+    @yield('footer')
+</body>
+</html>
