22baee1001551726394771f0712478a46f745ac3
ziszlai <ziszlai@gmail.com>
2017-01-03 15:01:29 +0200

    Authentification as admin

---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class MustBeAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = $request->user();
+        if ($user && $user->isAdmin() ) {
+            return $next($request);
+        }
+        abort(404, 'No way.');
+    }
+}
